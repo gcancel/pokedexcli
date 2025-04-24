0ca9774f9622b56229918d80392650060e3d8a61
gcancel <gmartin9892@gmail.com>
2025-04-23 20:57:00 -0500

    changing config back from testing

--- config.go
+++ config.go
@@ -1,24 +1,23 @@
 package main
 
-
-type PokeApiJsonResponse struct{
-	Count int `json:"count"`
-	Next string `json:"next"`
+type PokeApiJsonResponse struct {
+	Count    int    `json:"count"`
+	Next     string `json:"next"`
 	Previous string `json:"previous"`
-	Results []struct{
+	Results  []struct {
 		Name string `json:"name"`
-		URL string `json:"url"`
+		URL  string `json:"url"`
 	} `json:"results"`
 }
 
-type Config struct{
-	Next string
-	Prev string
+type Config struct {
+	Next  string
+	Prev  string
 	Count int
 }
 
 var APIConfig = Config{
-	Next: "https://pokeapi.co/api/v2/location-area?offset=1060&limit=20",
-	Prev: "",
+	Next:  "https://pokeapi.co/api/v2/location-area?offset=0&limit=20",
+	Prev:  "",
 	Count: 1089,
 }
